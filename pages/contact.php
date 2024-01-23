d6622662f5ced968c6201ed2e06e7c447ed99b3c
danoux10 <danybarbe10@gmail.com>
2024-01-23 23:20:08 +0100

    contact

--- pages/contact.php
+++ pages/contact.php
@@ -18,7 +18,10 @@
 	
 	<form action="">
 		<span>
-			<label for="nom" class="required">nom</label>
+			<label for="nom">
+				nom
+				<span class="required">*</span>
+			</label>
 			<input type="text" id="nom" placeholder="nom">
 		</span>
 		
@@ -28,27 +31,39 @@
 		</span>
 		
 		<span>
-			<label for="email" class="required">email</label>
+			<label for="email">
+				email
+				<span class="required">*</span>
+			</label>
 			<input type="email" id="email" placeholder="email">
 		</span>
 		
 		<span>
-			<label for="phone" class="required">téléphone</label>
+			<label for="phone">
+				téléphone
+				<span class="required">*</span>
+			</label>
 			<input type="text" id="phone" placeholder="téléphone">
 		</span>
 		
 		<span>
-			<label for="message" class="required">message</label>
+			<label for="message">
+				message
+				<span class="required">*</span>
+			</label>
 			<textarea name="" id="message" placeholder="message" cols="30" rows="10"></textarea>
 		</span>
-		
+		<span class="text">
+			<p class="required">*</p>
+			<p>champ requit</p>
+		</span>
 		<button type="submit">
 			<span class="icon-content">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
           <path d="M16.1 260.2c-22.6 12.9-20.5 47.3 3.6 57.3L160 376V479.3c0 18.1 14.6 32.7 32.7 32.7c9.7 0 18.9-4.3 25.1-11.8l62-74.3 123.9 51.6c18.9 7.9 40.8-4.5 43.9-24.7l64-416c1.9-12.1-3.4-24.3-13.5-31.2s-23.3-7.5-34-1.4l-448 256zm52.1 25.5L409.7 90.6 190.1 336l1.2 1L68.2 285.7zM403.3 425.4L236.7 355.9 450.8 116.6 403.3 425.4z"/>
 				</svg>
-				envoyer
 			</span>
+			envoyer
 		</button>
 	</form>
 </main>
